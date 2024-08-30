12f31690c316a4d834b9de7aa3e447816f8b7db3
Eugene17845 <razdolbai55h@gmail.com>
2024-08-30 20:12:16 +0300

    Add files via upload

---
+++ 30.08 Переменные.py
@@ -0,0 +1,9 @@
+name = 'Курс: '
+task = 'всего задач:'
+hour = 'затрачено часов:'
+hour_avr = 'среднее время выполнения '
+quantity = '12'
+hour_task = '1.5'
+print(name + 'Python', task, quantity, hour, hour_task, hour_avr,
+      (str(float(hour_task)/int(quantity))))
+
